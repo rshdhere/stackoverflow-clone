25bdb58e8acdc6f005ab81537b9611a312cf3bb9
raashed <mohammedraashed10@gmail.com>
2025-02-12 05:45:38 +0530

    eslint and prettier was installed and configured succcessfully

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ("next/core-web-vitals", "next/typescript", "prettier"),
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"]
+    }
+  })
 ];
 
 export default eslintConfig;
